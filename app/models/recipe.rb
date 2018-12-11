eb469b3c179cb2e65d19ebf8b1af1d3c6045e885
Taylor Williams <taylor@thewilliams.net>
2018-12-11 00:33:23 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ing_at)
+    self.ingredients << Ingredient.find_or_create_by(name: ing_at[:name], quantity: ing_at[:quantity])
+  end
 end
